56aef1db164a1502b95eac1e0c357c9b2709ec1c
SaiSandeepGollapudi <gsandeep2406@gmail.com>
2024-05-21 10:43:58 -0400

    spring security

--- src/main/java/com/companyname/springsecOAUTH2GitHub/controller/SecureController.java
+++ src/main/java/com/companyname/springsecOAUTH2GitHub/controller/SecureController.java
@@ -12,9 +12,9 @@ public class SecureController {
     //This is a simple MVC controller.This is not REST API.
 @GetMapping("/")// This is an springMVC path with the path"/" like if someone is trying to access my local host with the Port 8080, this is the path that will get invoked
     public String main(OAuth2AuthenticationToken token){//inside this token we'll have all the details of the end user that I received from the GitHubAUTH server.
-        System.out.println(token.getPrincipal());
-        return "secure.html";
-// if he wants to access this he won't directly get the secure.html.He will be redirected to the GitHub login page. Once he entered his actual credentials, then only he will
+        System.out.println(token.getPrincipal());//his account ID, authorities that my end user has  like ROLE_USER, scope of read:user.  there is an avatar URL using which you can
+//load the profile picture of my end user.And there is also a followers URL like using this followers URL anyone can understand how many followers I have.
+return "secure.html";//if he wants to access this he won't directly get secure.html.He will be redirected to  GitHub login page. Once he entered his actual credentials, then only he will
 // be able to access this secure.html.
 
 }
